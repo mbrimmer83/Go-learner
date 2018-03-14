8d622dd60ce553b146af0a032aaa056e8965ae3b
Matthew Brimmer <mbrimmer1@gmail.com>
2018-03-14 16:02:20 -0400

    Initial commit

---
+++ deck.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+type deck []string
+
+// Reciever function of type deck
+// any variable of type deck get access to the print method
+// d is similar to this in JS
+// references the instance of the type variable
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Hearts", "Spades", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+var card string
+
+func main() {
+	cards := newDeck()
+
+	cards.saveToFile("my_cards")
+}
